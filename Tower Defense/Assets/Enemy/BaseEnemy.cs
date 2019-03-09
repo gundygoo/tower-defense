3addce20003065254f7403d44c49569089b9161c
Erik <gundy.goo@gmail.com>
2019-03-09 12:46:17 -0600

    Updated base classes.

--- Tower Defense/Assets/Enemy/BaseEnemy.cs
+++ Tower Defense/Assets/Enemy/BaseEnemy.cs
@@ -22,20 +22,19 @@ public abstract class BaseEnemy : MonoBehaviour,  IBaseEnemy
         Move();
     }
 
-    //Base imp of Init - Should be overridden in child class
-    public void Init()
-    {
-        throw new System.NotImplementedException();
-    }
+    //Move the Enemy
+    protected abstract void Move();
 
-    //Base imp of Move - Should be overridden in child class
-    public void Move()
-    {
-        throw new System.NotImplementedException();
-    }
+    //Init the Enemy
+    protected abstract void Init();
 
     //Base imp of UpdateHealth - Should be overridden in child class
     public void UpdateHealth(int updateHealthAmount)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateSpeed(int updateSpeedAmount)
     {
         throw new System.NotImplementedException();
     }
--- Tower Defense/Assets/Enemy/IBaseEnemy.cs
+++ Tower Defense/Assets/Enemy/IBaseEnemy.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public interface IBaseEnemy
 {
-    //Do the init things
-    void Init();
-
-    //Do the move things
-    void Move();
-
     //Do the health update things
     void UpdateHealth(int updateHealthAmount);
+
+    //Do the speed update things
+    void UpdateSpeed(int updateSpeedAmount);
 }
