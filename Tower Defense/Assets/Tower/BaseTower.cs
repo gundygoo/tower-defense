3956265bdcd5e3a298ee23986ca8cb56d0087f58
Patrick Oudemans <poudemans@gmail.com>
2019-03-09 12:58:54 -0600

    Added base tower and two tower children.

---
+++ Tower Defense/Assets/Tower/BaseTower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTower : MonoBehaviour
+{
+    // The damage of the tower
+    protected int damage;
+
+    // The range of the tower
+    protected int range;
+
+    // The rate of fire of the tower
+    protected int rateOfFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Attack the enemy
+    public enum Attack
+    {
+
+    }
+}
---
+++ Tower Defense/Assets/Tower/NonTargetedTower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonTargetedTower : BaseTower
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IBaseEnemy NonTarget()
+    {
+        throw new System.NotImplementedException();
+    }
+}
