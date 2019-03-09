ea8332bebb39e0ba1024a11704e6e1094e595597
Erik <gundy.goo@gmail.com>
2019-03-09 12:39:14 -0600

    Added base enemy classes.

---
+++ Tower Defense/Assets/Enemy/BaseEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour,  IBaseEnemy
+{
+    //The health of the enemy
+    protected int health;
+
+    //The speed of the enemy
+    protected int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //Base imp of Init - Should be overridden in child class
+    public void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Base imp of Move - Should be overridden in child class
+    public void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Base imp of UpdateHealth - Should be overridden in child class
+    public void UpdateHealth(int updateHealthAmount)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Tower Defense/Assets/Enemy/IBaseEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBaseEnemy
+{
+    //Do the init things
+    void Init();
+
+    //Do the move things
+    void Move();
+
+    //Do the health update things
+    void UpdateHealth(int updateHealthAmount);
+}
