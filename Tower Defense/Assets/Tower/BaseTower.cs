d2156bca616e886edd337bb8cf2089b61bcb4bf9
Patrick Oudemans <poudemans@gmail.com>
2019-03-09 13:50:28 -0600

    Added placeholder sprites and sliced the health sprite

--- Tower Defense/Assets/Tower/BaseTower.cs
+++ Tower Defense/Assets/Tower/BaseTower.cs
@@ -18,7 +18,7 @@ public class BaseTower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        attack = Attack.Fire;
+        attack = Attack.Basic;
     }
 
     // Update is called once per frame
